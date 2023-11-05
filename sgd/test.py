f3ef9e5a780ebd245f01fe27f408dfc5b03dc9b5
lml-thinkbook <lml20020122@126.com>
2023-11-06 03:49:32 +0800

    xxx

---
+++ sgd/test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+def main():
+    print(".")
+    fig, axs = plt.subplots(2, 2, figsize=(4, 3), layout='constrained')
+    print("helloworld")
+
+
+
+if __name__ == "__main__":
+    main()
